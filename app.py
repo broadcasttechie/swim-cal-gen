b8f5eae8c600d4d5e05b9cbd097d5c9e16de090a
broadcasttechie <kristan@webbitpro.co.uk>
2024-06-12 11:57:18 +0100

    add events

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask
+import requests
+import json
+
 
 app = Flask("app")
 
@@ -6,3 +9,25 @@ app = Flask("app")
 def index():
     return "Hello World!"
 
+
+
+@app.route("/events")
+def events():
+    url = 'https://birminghamleisure.legendonlineservices.co.uk/birmingham_comm_rg_home/Timetable/GetClassTimeTable'
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    payload = {
+        'ResourceSubTypeIdList': '469',
+        #'ResourceSubTypeIdList': '537',
+        'FacilityLocationIdList': '4811',
+        'DateFrom': '2024-06-13T00:00:00+01:00',
+        'DateTo': '2024-06-14T00:00:00+01:00'
+    }
+
+    json_payload = json.dumps(payload)
+    #print(json_payload)
+    response = requests.post(url, headers=headers, data=json_payload)
+    for event in  response.json()['Results']:
+        print(event['title'])
+    return response.json()['Results']
